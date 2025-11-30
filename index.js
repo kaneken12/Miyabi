80e875ed5613d5a106b49f4217fc4e0720c5fa91
Chrysler-Lexus <kenkaneky244@gmail.com>
2025-11-30 16:45:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const fs = require('fs');
 
 const MiyabiBot = require('./src/bot/miyabibot');
+const MiyabiBot = require('./src/utils/helpers');
 const Database = require('./src/database/db');
 
 class WhatsAppClient {
